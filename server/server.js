88e5eb89a26c5f50375bfe05e760d420a293d79a
Ashutosh17042004 <ashutoshkumarsinghh17a@gmail.com>
2025-11-19 21:45:10 +0530

    Socket.io connection handler

--- server/server.js
+++ server/server.js
@@ -5,10 +5,36 @@ import http from "http";
 import { connectDB } from "./lib/db.js";
 import userRouter from "./Routes/userRoutes.js";
 import messageRouter from "./Routes/messageRoutes.js";
+import { Server } from "socket.io";
 
 //Create express app and HTTP server
 const app = express();
 const server = http.createServer(app);
+
+// initialize socket.io server
+export const io = new Server(server, {
+  cors: { origin: "*" },
+});
+
+// store online users
+export const userSocketMap = {}; //{userId:socketId}
+
+// Socket.io connection handler
+io.on("connection", (socket) => {
+  const userId = socket.handshake.query.userId;
+  console.log("User connected", userId);
+  if (userId) userSocketMap[userId] = socket.id;
+
+  //Emit online users to all connected clients
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected", userId);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    
+  });
+});
 
 // Middleware
 app.use(express.json({ limit: "4mb" }));
