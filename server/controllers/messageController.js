2c5be4231140d5df554c91e65925de5d18cfebba
Ashutosh17042004 <ashutoshkumarsinghh17a@gmail.com>
2025-11-19 16:11:38 +0530

    create controller func for left sidebar for displaying other users and there unseen messages

---
+++ server/controllers/messageController.js
@@ -0,0 +1,32 @@
+import Message from "../models/message.js";
+import User from "../models/User.js";
+
+// get all user expect the logged in user
+export const getUserForSidevbar = async (req, res) => {
+  try {
+    const userId = req.user_Id;
+    const filteredUser = await User.find({ _id: { $ne: userId } }).select(
+      "-password"
+    ); //here $ne=(not equal to)
+
+    // count no. of mesg not seen
+    const unseenMessages = {};
+    const promise = filteredUser.map(async (user) => {
+      const messages = await Message.find({
+        sendenId: user._id,
+        recieverId: userId,
+        seen: false,
+      });
+      if (messages.length > 0) {
+        unseenMessages[user._id] = messages.length;
+      }
+    });
+    await promise.all(promises);
+    res.json({ success: true, users: filteredUser, unseenMessages });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// get all messages for selected user
