679b418e9be722bade05354570fed7a913456e5a
Ashutosh17042004 <ashutoshkumarsinghh17a@gmail.com>
2025-11-19 17:06:27 +0530

    controller for send message to selected user

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,5 +1,6 @@
 import Message from "../models/message.js";
 import User from "../models/User.js";
+import cloudinary from "../lib/cloudinary.js";
 
 // get all user expect the logged in user
 export const getUserForSidebar = async (req, res) => {
@@ -64,6 +65,31 @@ export const markMessageAsSeen = async (req, res) => {
     const { id } = req.params;
     await Message.findByIdAndUpdate(id, { seen: true });
     res.json({ success: true });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// send message to selected user
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const receiverId = req.params.id;
+    const senderId = req.user._id;
+
+    let imageUrl;
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+    const newMessage = Message.create({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+    res.json({ success: true, newMessage });
   } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: error.message });
