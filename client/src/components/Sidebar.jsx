bb80c8277b10e8dd4dbd78b85ca0c375d52ce21e
Ashutosh17042004 <ashutoshkumarsinghh17a@gmail.com>
2025-11-21 19:02:06 +0530

    added func for filter user subsribe or unsubscride

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,11 +1,41 @@
-import React, { useContext } from "react";
-import assets, { userDummyData } from "../assets/assets";
+import React, { useContext, useEffect, useState } from "react";
+import assets from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
+import { ChatContext } from "../../context/ChatContext";
 
-const Sidebar = ({ selectedUser, setSelectedUser }) => {
-  const { logout } = useContext(AuthContext);
+const Sidebar = () => {
+  const {
+    getUsers,
+    users,
+    selectedUser,
+    setSelectedUser,
+    unseenMessages,
+    setUnseenMessages,
+  } = useContext(ChatContext);
+
+  const { logout, onlineUser,authUser } = useContext(AuthContext);
+
+  const [input, setInput] = useState(false);
+
   const navigate = useNavigate();
+
+  // const filteredUsers = input
+  //   ? users.filter((user) =>
+  //       user.fullName.toLowerCase().includes(input.toLowerCase())
+  //     )
+  //   : users;
+  const filteredUsers = input
+    ? users
+        .filter((user) => user._id !== authUser?._id)
+        .filter((user) =>
+          user.fullName.toLowerCase().includes(input.toLowerCase())
+        )
+    : users.filter((user) => user._id !== authUser?._id);
+
+  useEffect(() => {
+    getUsers();
+  }, [onlineUser]);
 
   return (
     <div
@@ -41,6 +71,7 @@ const Sidebar = ({ selectedUser, setSelectedUser }) => {
         <div className="bg-[#282142] rounded-full flex items-center gap-2 py-3 px-4 mt-5">
           <img src={assets.search_icon} alt="Search" className="w-3" />
           <input
+            onChange={(e) => setInput(e.target.value)}
             type="text"
             className="bg-transparent border-none outline-none text-white text-xs placeholder:-[#c8c8c8] flex-1"
             placeholder="Search User..."
@@ -48,7 +79,7 @@ const Sidebar = ({ selectedUser, setSelectedUser }) => {
         </div>
       </div>
       <div className="flex flex-col">
-        {userDummyData.map((user, index) => (
+        {filteredUsers.map((user, index) => (
           <div
             onClick={() => {
               setSelectedUser(user);
@@ -65,15 +96,15 @@ const Sidebar = ({ selectedUser, setSelectedUser }) => {
             />
             <div className="flex flex-col leading-5">
               <p>{user.fullName}</p>
-              {index < 3 ? (
+              {onlineUser.includes(user._id) ? (
                 <span className="text-green-400 text-xs"> online</span>
               ) : (
                 <span className="text-neutral-400 text-xs">offline </span>
               )}
             </div>
-            {index > 2 && (
+            {unseenMessages[user._id] > 0 && (
               <p className="absolute top-4 right-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-violet-500/50">
-                {index}
+                {unseenMessages[user._id]}
               </p>
             )}
           </div>
