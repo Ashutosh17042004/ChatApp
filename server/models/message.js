87ef91782bef78ea906754c121fd9afc10aeaa95
Ashutosh17042004 <ashutoshkumarsinghh17a@gmail.com>
2025-11-22 17:45:35 +0530

    change in message schema

--- server/models/message.js
+++ server/models/message.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const messageSchema = new mongoose.Schema(
   {
-    sendenId: {
+    senderId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
